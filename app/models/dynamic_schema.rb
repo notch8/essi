91db2edb68a4142089b2e03b679b2e1878feff64
Kiah <kiah@notch8.com>
2019-09-06 11:06:40 -0700

    refine dynamic_schema model / db table, update schema

--- app/models/dynamic_schema.rb
+++ app/models/dynamic_schema.rb
@@ -1,4 +1,8 @@
 class DynamicSchema < ApplicationRecord
   belongs_to :m3_context
   belongs_to :m3_profile
+
+  serialize :schema, JSON
+
+  delegate :profile_version, to: :m3_profile
 end
--- db/migrate/20190906005616_create_dynamic_schemas.rb
+++ db/migrate/20190906005616_create_dynamic_schemas.rb
@@ -2,11 +2,10 @@ class CreateDynamicSchemas < ActiveRecord::Migration[5.1]
   def change
     create_table :dynamic_schemas do |t|
       t.integer :version
-      t.string :m3class
+      t.string :m3_class
       t.references :m3_context, foreign_key: true
       t.references :m3_profile, foreign_key: true
-      t.integer :profile_version
-      t.json :schema
+      t.text :schema
 
       t.timestamps
     end
