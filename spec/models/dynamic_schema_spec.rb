e26852e9dc593014168afd840674568c9f85027d
Kiah <kiah@notch8.com>
2019-09-06 17:39:13 -0700

    flexible metadata model factories / specs

--- spec/models/dynamic_schema_spec.rb
+++ spec/models/dynamic_schema_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe DynamicSchema, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs_to a m3_context' do
+      assc = described_class.reflect_on_association(:m3_context)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it 'belongs_to a m3_profile' do
+      assc = described_class.reflect_on_association(:m3_profile)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
+
+  it 'is valid' do
+    dynamic_schema = FactoryBot.build(:dynamic_schema)
+    expect(dynamic_schema).to be_valid
+  end
 end
