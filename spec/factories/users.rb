f9c79507141012e43b8652139bd830c9f5990767
shana <shana@notch8.com>
2019-09-16 11:23:53 -0700

    update user factory to use traits

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,36 +5,36 @@ FactoryBot.modify do
     sequence(:email) { |n| "email-#{srand}@test.com" }
     provider { 'cas' }
 
-    factory :admin do
+    trait :admin do
       roles { [Role.where(name: 'admin').first_or_create] }
     end
 
-    factory :authorized_patron do
+    trait :authorized_patron do
       # All CAS users are authorized patrons.
     end
 
-    factory :image_editor do
+    trait :image_editor do
       roles { [Role.where(name: 'image_editor').first_or_create] }
     end
 
-    factory :editor do
+    trait :editor do
       roles { [Role.where(name: 'editor').first_or_create] }
     end
 
-    factory :fulfiller do
+    trait :fulfiller do
       roles { [Role.where(name: 'fulfiller').first_or_create] }
     end
 
-    factory :curator do
+    trait :curator do
       roles { [Role.where(name: 'curator').first_or_create] }
     end
 
-    factory :complete_reviewer do
+    trait :complete_reviewer do
       email { 'complete@example.com' }
       roles { [Role.where(name: 'notify_complete').first_or_create] }
     end
 
-    factory :takedown_reviewer do
+    trait :takedown_reviewer do
       email { 'takedown@example.com' }
       roles { [Role.where(name: 'notify_takedown').first_or_create] }
     end
