56c6ddbb15f6ae71a27c0cb43077db4e39e1cd1f
Julie Allinson <julie@notch8.com>
2019-09-27 16:02:18 -0700

    add db migrations and models for profiles

---
+++ db/migrate/20190925233215_create_m3_profile_properties.rb
@@ -0,0 +1,14 @@
+class CreateM3ProfileProperties < ActiveRecord::Migration[5.1]
+  def change
+    create_table :m3_profile_properties do |t|
+      t.string :name # @todo required
+      t.string :property_uri
+      t.string :cardinality_minimum
+      t.string :cardinality_maximum
+      t.string :indexing # serialize as array
+      t.references :m3_profile, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
