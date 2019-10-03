cfc78a3d1606a32d3a32f4cbafdf27125ad481f0
Kiah <kiah@notch8.com>
2019-10-03 13:51:39 -0700

    rename 'type' column on M3Profile table to avoid ActiveRecord::SubclassNotFound error

--- db/migrate/20190906004146_create_m3_profiles.rb
+++ db/migrate/20190906004146_create_m3_profiles.rb
@@ -3,12 +3,12 @@ class CreateM3Profiles < ActiveRecord::Migration[5.1]
     create_table :m3_profiles do |t|
       t.string :name
       t.integer :profile_version # version in m3
-      t.text :profile
       t.string :m3_version
       t.string :responsibility
       t.string :responsibility_statement
       t.string :date_modified
-      t.string :type
+      t.string :profile_type
+      t.text :profile
 
       t.timestamps
     end
