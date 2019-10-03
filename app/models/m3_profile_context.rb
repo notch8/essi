9e7d82c8926fc8fd0aa7b07e3581f35b9a3572f9
Julie Allinson <julie@notch8.com>
2019-10-03 13:21:23 +0100

    change texts relationship on classes and contexts to avoid confusion, make cardinality fields integers

--- app/models/m3_profile_context.rb
+++ app/models/m3_profile_context.rb
@@ -1,4 +1,4 @@
 class M3ProfileContext < ApplicationRecord
   belongs_to :m3_profile
-  has_many :texts, as: :textable, class_name: 'M3ProfileText'
+  has_many :context_texts, as: :textable, class_name: 'M3ProfileText'
 end
