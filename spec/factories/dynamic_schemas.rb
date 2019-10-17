29f0c7984f0f9517d2a0a991122b9f7fd98f37e8
Julie Allinson <julie@notch8.com>
2019-10-17 13:24:39 +0100

    DynamicSchemaService with methods to return key information necessary for creating, displaying and searching works (#32)
    This may require refactoring during work to add to Hyrax classes.

--- spec/factories/dynamic_schemas.rb
+++ spec/factories/dynamic_schemas.rb
@@ -1,8 +1,26 @@
 FactoryBot.define do
   factory :dynamic_schema, class: M3::DynamicSchema do
-    m3_class   { "FlexibleWork" }
-    m3_context { FactoryBot.build(:m3_context) }
+    m3_class   { 'FlexibleWork' }
+    m3_context { FactoryBot.build(:m3_context_assigned) }
     m3_profile { FactoryBot.build(:m3_profile) }
-    schema     { { "version": 1, "classes":"FlexibleWork", "contexts":"flexible_context" } }
+    schema     do
+      {
+        'type' => 'http://example.com/classes/FlexibleWork',
+        'display_label' => 'Flexible Metadata Example',
+        'properties' => [
+          { 'title' =>
+            {
+              'predicate' => 'http://purl.org/dc/terms/title',
+              'display_label' => 'Title in Context',
+              'required' => true,
+              'singular' => false,
+              'indexing' => %w[
+                stored_searchable
+                facetable
+              ]
+            } }
+        ]
+      }
+    end
   end
 end
