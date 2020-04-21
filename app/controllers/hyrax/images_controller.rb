486ebdd36cf1378b962ba8707ddd739016c08280
Julie Allinson <julie@notch8.com>
2020-04-21 15:37:03 +0100

    works configured - image only

--- app/controllers/hyrax/images_controller.rb
+++ app/controllers/hyrax/images_controller.rb
@@ -11,6 +11,8 @@ module Hyrax
     include ESSI::BreadcrumbsForWorks
     include ESSI::OCRSearch
     include ESSI::StructureBehavior
+
+    include FlexibleMetadata::DynamicControllerBehavior
     self.curation_concern_type = ::Image
 
     # Use this line if you want to use a custom presenter
