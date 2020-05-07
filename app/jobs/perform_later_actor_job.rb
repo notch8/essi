127fea7fa71fad5e5cc221a811d06e70ddccc4c8
Julie Allinson <julie@notch8.com>
2020-05-07 16:10:40 +0100

    perform later issue

--- app/jobs/perform_later_actor_job.rb
+++ app/jobs/perform_later_actor_job.rb
@@ -1,8 +1,6 @@
 class PerformLaterActorJob < ActiveJob::Base
   def perform(action, curation_concern, ability_user, attributes_for_actor)
     # Rebuild the actor environment
-    # JA - remove the in_perform_later_actor_job attribute
-    attributes_for_actor.delete 'in_perform_later_actor_job'
     env = Hyrax::Actors::Environment.new(curation_concern.constantize.new, ::Ability.new(ability_user), attributes_for_actor)
     Hyrax::CurationConcern.actor.send(action, env)
   end
